3ac12b55018ed4ae21231457deef7d1c306964d6
vietvd13 <vuducviet0131@gmail.com>
2021-05-26 12:04:29 +0700

    [Bot Duck] Change remove folder bot

--- main.py
+++ main.py
@@ -1,24 +1,24 @@
 import discord
 from discord.ext import commands
-import youtube_dl
+from discord.ext.commands import bot
 import os
 
-client = commands.Bot(command_prefix = "v ")
+bot = commands.Bot(command_prefix = "v ")
 
-@client.event
+@bot.event
 async def on_ready():
     print("Con vịt chạy lạch bà lạch bạch")
 
-@client.event
+@bot.event
 async def on_member_join(member):
     print(f'{member} đã gia nhập vào đàn vịt thế giới!')
 
-@client.command()
+@bot.command()
 async def ping(ctx):
-    await ctx.send(f'Thưa ngài chuồng vịt bị delay: {round(client.latency * 1000)}ms.')
+    await ctx.send(f'Thưa ngài chuồng vịt bị delay: {round(bot.latency * 1000)}ms.')
 
-@client.command()
+@bot.command()
 async def chao(ctx):
     await ctx.send(f"Xin chào bạn vịt tên: {ctx.author.name}!")
-
-client.run('ODQ2NzQ5Njg5MDY3MzM5ODU2.YK0DGQ.PJxU6ARD3mMU9SlOZMdq0VPqHqc')
+    
+bot.run('ODQ2NzQ5Njg5MDY3MzM5ODU2.YK0DGQ.PJxU6ARD3mMU9SlOZMdq0VPqHqc')
